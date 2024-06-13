7c297613893bc4328e3c1ee8d80c868617214728
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-13 22:28:49 +0900

    mainText追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export function Header() {
   return (
     <header className="bg-white dark:bg-zinc-800 p-4 flex justify-between items-center">
+        <h1 className="text-xl font-semibold text-zinc-900 dark:text-zinc-100">My Website</h1>
       </header>
   )
 }
