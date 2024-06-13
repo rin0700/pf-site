0d06eff6934dd844e8e399b58caa7de0f6457a5e
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-13 22:28:16 +0900

    headerタグ追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export function Header() {
   return (
+    <header className="bg-white dark:bg-zinc-800 p-4 flex justify-between items-center">
+      </header>
   )
 }
 
