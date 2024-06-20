0f2e23687542da73a466566a5f476e5e8e9a38aa
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-20 14:16:31 +0900

    title/description追加

--- src/components/LanguageCard.jsx
+++ src/components/LanguageCard.jsx
@@ -7,6 +7,15 @@ function LanguageCard() {
   return (
     <section id="card1" class="card">
       <img src={imageHTML} alt="HTML" />
+      <div class="card__content">
+        <p class="card__title">Lorem Ipsum</p>
+        <p class="card__description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae
+          justo vel lorem tincidunt ultrices at non nunc. Donec in sapien viverra,
+          tincidunt augue id, efficitur massa.
+        </p>
+      </div>
+    </section>
 
   )
 }
