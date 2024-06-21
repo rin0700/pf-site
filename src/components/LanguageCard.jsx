754f5b6e443f88a6211f34ab5151747d5f17f70e
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-21 23:13:29 +0900

    importImage

--- src/components/LanguageCard.jsx
+++ src/components/LanguageCard.jsx
@@ -1,7 +1,25 @@
-
-import React from 'react'
-import '../Style/LanguageCard.css'
-import imageHTML from '../img/html.svg'
+import React from 'react';
+import '../Style/LanguageCard.css';
+import imageAWS from '../img/amazon.svg';
+import imageBlender from '../img/Blender.svg';
+import imageCSharp from '../img/CSharp.svg';
+import imageCanva from '../img/Canva.svg';
+import imageCSS from '../img/css.svg';
+import imageDocker from '../img/docker.svg';
+import imageFigma from '../img/Figma.svg';
+import imageGithub from '../img/github.svg';
+import imageHTML from '../img/html.svg';
+import imageJava from '../img/java.svg';
+import imageJS from '../img/javascript.svg';
+import imageJquery from '../img/jquery.svg';
+import imageMySQL from '../img/mysql.svg';
+import imagePhp from '../img/php.png';
+import imageReact from '../img/react.svg';
+import imageSwift from '../img/Swift.svg';
+import imageSwiftUI from '../img/SwiftUI.svg';
+import imageUnity from '../img/Unity.svg';
+import imageVsCode from '../img/vscode.svg';
+import imageXCode from '../img/XCode.svg';
 
 function LanguageCard() {
   return (
