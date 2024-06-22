fb0cac09f0e3a6e4d84f7b9eeb1ef575f5677a16
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-23 00:55:22 +0900

    hardSkill用関数

--- src/components/SkillRadarChart.jsx
+++ src/components/SkillRadarChart.jsx
@@ -3,3 +3,13 @@ import '../Style/SkillRadarChart.css';
 import {
   Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Legend, ResponsiveContainer
 } from 'recharts';
+
+const hardSkill = [
+  { subject: 'SwiftUI', A: 5, fullMark: 5 },
+  { subject: 'HTML', A: 4, fullMark: 5 },
+  { subject: 'Java', A: 4, fullMark: 5 },
+  { subject: 'Unity', A: 5, fullMark: 5 },
+  { subject: 'Swift', A: 2, fullMark: 5 },
+  { subject: 'JavaScript', A: 3, fullMark: 5 },
+  { subject: 'C#', A: 3, fullMark: 5 },
+];
