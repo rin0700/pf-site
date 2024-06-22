ae4a1e26102ea79d6f13741c099a8984e8cedb4d
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-23 00:54:44 +0900

    import文

---
+++ src/components/SkillRadarChart.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import '../Style/SkillRadarChart.css';
+import {
+  Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Legend, ResponsiveContainer
+} from 'recharts';
