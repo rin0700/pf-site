3c5050b975a47e0541c8a16453b2727d400305a7
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-24 01:13:56 +0900

    figmaText

--- src/components/LanguageCard.jsx
+++ src/components/LanguageCard.jsx
@@ -138,7 +138,7 @@ const cards = [
     id: 'blender',
     imgSrc: imageBlender,
     title: 'Blender',
-    description: 'Blender is a free and open-source 3D computer graphics software toolset used for creating animated films, visual effects, art, 3D printed models, motion graphics, interactive 3D applications, virtual reality, and computer games.',
+    description: '3Dモデリングやアニメーションを趣味で行なっていました。',
     category: 'ToolsAndEnvironments'
   },
   {
