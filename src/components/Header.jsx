10153db2895ffbc004e23eafb3ae8ee531f5ec5e
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-28 02:28:43 +0900

    UI調整

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,14 @@ import { ReactComponent as ProfileIcon } from '../img/ProfileIcon.svg';
 import { ReactComponent as UnityIcon } from '../img/UnityIcon.svg';
 import { ReactComponent as ContactIcon } from '../img/ContactIcon.svg';
 import { ReactComponent as PortfolioIcon } from '../img/PortfolioIcon.svg';
+
+function Header() {
   return (
+    <>
+      <div className="header-spacer" />
+      <header className="header-container">
+        <h1 className="header-title">My Website</h1>
+        <nav className="header-nav">
           <button className="Btn">
             Profile
             <ProfileIcon className="svg" />
@@ -22,12 +29,10 @@ import { ReactComponent as PortfolioIcon } from '../img/PortfolioIcon.svg';
             Works
             <PortfolioIcon className="svg" />
           </button>
-      </nav>
-    </header>
-  )
+        </nav>
+      </header>
+    </>
+  );
 }
 
-export default Header
-
-
-
+export default Header;
