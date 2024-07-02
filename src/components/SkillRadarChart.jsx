b534fad87938784a10a5e6b6cfcace2183c4bca7
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-07-02 19:42:03 +0900

    fix/Swift

--- src/components/SkillRadarChart.jsx
+++ src/components/SkillRadarChart.jsx
@@ -9,7 +9,7 @@ const hardSkill = [
   { subject: 'HTML', A: 4, fullMark: 5 },
   { subject: 'Java', A: 4, fullMark: 5 },
   { subject: 'Unity', A: 5, fullMark: 5 },
-  { subject: 'Swift', A: 2, fullMark: 5 },
+  { subject: 'Swift', A: 3, fullMark: 5 },
   { subject: 'JavaScript', A: 3, fullMark: 5 },
   { subject: 'C#', A: 3, fullMark: 5 },
 ];
