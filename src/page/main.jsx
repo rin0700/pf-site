7c0f4a58e21a780545e9f972cc2f32d4503dbb24
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-09-20 19:57:19 +0900

    fix/syntax error

--- src/page/main.jsx
+++ src/page/main.jsx
@@ -4,7 +4,7 @@ import Profile from '../components/Profile';
 import LanguageCard from '../components/LanguageCard';
 import Qualifications from '../components/Qualifications';
 import SkillRadarChart from '../components/SkillRadarChart';
-import Sns from '../components/Sns';
+import Sns from '../components/sns';
 
 function main() {
   return (
