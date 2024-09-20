751c29b747c5de5979299863df9c57d0902ffc65
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-09-20 19:51:56 +0900

    リンク機能の実装

--- src/components/sns.jsx
+++ src/components/sns.jsx
@@ -4,3 +4,24 @@ import note from '../img/note.png';
 import qiita from '../img/qiita.png';
 import StudyPlus from '../img/StudyPlus.svg';
 import '../Style/Sns.css';
+  const handleXClick = () => {
+    window.location.href = 'https://x.com/Liy_SysE';
+  };
+  const handleGithubClick = () => {
+    window.location.href = 'https://github.com/rin0700';
+  };
+  const handleDiscordClick = () => {
+    window.location.href = 'https://discord.gg/bH26vGUx';
+  };
+  const handleVRChatClick = () => {
+    window.location.href = 'https://vrchat.com/home/user/usr_c08af74f-c1bc-4ba6-b4d2-ba0eeeec7844';
+  };
+  const handleNoteClick = () => {
+    window.location.href = 'https://note.com/rin0700';
+  }
+  const handleQiitaClick = () => {
+    window.location.href = 'https://qiita.com/rin0700';
+  }
+  const handleStudyPlusClick = () => {
+    window.location.href = 'https://app.studyplus.jp/users/8b5909e6b2304dfb8590dbca5f04fd81';
+  }
