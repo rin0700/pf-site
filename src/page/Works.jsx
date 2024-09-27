ff8b5deeaadba301f7a4e024125f6ef783fee7b2
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-09-27 10:01:58 +0900

    fureture/Sheet

--- src/page/Works.jsx
+++ src/page/Works.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Sheet from '../components/Sheet';
 
 function Works() {
   return (
     <>
       <Header />
-      <h1 className="center">製作中のページです。</h1>
+      <Sheet />
     </>
   );
 }
