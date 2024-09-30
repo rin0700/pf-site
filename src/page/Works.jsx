f6f03104f2ad29d5102bd823c3115a0442d9074d
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-09-30 10:15:15 +0900

    import blog

--- src/page/Works.jsx
+++ src/page/Works.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Sheet from '../components/Sheet';
+import Blog from '../components/Blog';
 
 function Works() {
   return (
     <>
       <Header />
       <Sheet />
+      <Blog />
     </>
   );
 }
