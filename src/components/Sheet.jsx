53dfabafb3c33279660e3845f4a2f8b50c4c4a0e
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-09-30 10:13:55 +0900

    fix/className

--- src/components/Sheet.jsx
+++ src/components/Sheet.jsx
@@ -5,7 +5,7 @@ import '../Style/Sheet.css';
 function Sheet() {
   return (
     <>
-      <h2 className='textCenter'>PORTFOLIO</h2>
+      <h2 className='textCenter'>Portfolio</h2>
       <div className="iframeWrappers">
         <iframe
           src="https://docs.google.com/presentation/d/1gIyTjE6vPznTC44LGMvR0xmGr0OyUNRiC95ubga8OCA/embed?start=false&loop=false&delayms=3000"
@@ -17,25 +17,27 @@ function Sheet() {
       </div>
       <h2 className="textCenter">Artificial Intelligence</h2>
       <div className="iframeContainers">
-        <div className="iframeWrappers smallIframe">
+        <div className="smallIframe">
           <iframe
             src="https://docs.google.com/presentation/d/1dz4m5v4dVLonfUxSvymLLkg-OJ9aTRbUVoYX1oIrljs/embed?start=false&loop=false&delayms=3000"
             frameBorder="0"
             allowFullScreen={true}
             mozallowfullscreen="true"
-            webkitallowfullscreen="true">
+            webkitallowfullscreen="true"
+          >
           </iframe>
         </div>
-        <div className="iframeWrappers smallIframe">
+        <div className="smallIframe">
           <iframe
             src="https://docs.google.com/presentation/d/1LkDKT6JHQmmmSlvNGk2v_f51pwVXgNRO_RoWJ9zkf2M/embed?start=false&loop=false&delayms=3000"
             frameBorder="0"
             allowFullScreen={true}
             mozallowfullscreen="true"
-            webkitallowfullscreen="true">
+            webkitallowfullscreen="true"
+          >
           </iframe>
         </div>
-      </div>
+      </div >
     </>
   );
 }
