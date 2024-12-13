ebaeefd9fd14c88aa24b9283f71986854b3f4a41
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-12-13 10:19:14 +0900

    index.htmlの呼び出し

---
+++ src/page/Metaverse.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//http://localhost:3000/metaverse/
+
+function Metaverse() {
+  return (
+    <div>
+      <iframe
+        width="1200"
+        height="780"
+        src="/metaverse/index.html"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
+
+export default Metaverse;
