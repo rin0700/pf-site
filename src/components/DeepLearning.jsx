fccde2116c1eba1f97dddad946f820c348ae74c6
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-12-22 19:30:07 +0900

    ウィンドウ表示のAPI取得、配置

--- src/components/DeepLearning.jsx
+++ src/components/DeepLearning.jsx
@@ -1,5 +1,28 @@
 /* eslint-disable prettier/prettier */
 import React, { useState, useRef } from 'react';
+  const startScreenShare = async () => {
+    try {
+      stream = await navigator.mediaDevices.getDisplayMedia({
+        video: true
+      });
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+      }
+      setIsSharing(true);
+    } catch (err) {
+      console.error("画面共有に失敗しました。", err);
+    }
+  };
+
+  const stopScreenShare = () => {
+    if (stream) {
+      const tracks = stream.getTracks();
+      tracks.forEach(track => track.stop());
+      setIsSharing(false);
+    }
+  };
+
   return (
     <div>
       <h1>画面共有テスト</h1>
