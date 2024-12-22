73912488648663eadda4048950658d324685550e
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-12-22 19:27:21 +0900

    DeepLearningコンポーネントをコメントアウト

--- src/page/Works.jsx
+++ src/page/Works.jsx
@@ -3,11 +3,13 @@ import Header from '../components/Header';
 import Sheet from '../components/Sheet';
 import Blog from '../components/Blog';
 import Vrc from '../components/Vrc';
+// import DeepLearning from '../components/DeepLearning';
 
 function Works() {
   return (
     <>
       <Header />
+      {/* <DeepLearning /> */}
       <Sheet />
       <Blog />
       <Vrc />
