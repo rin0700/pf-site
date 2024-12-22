d7a19bc36cddde688661887c6ca9fe3bf96dbd58
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-12-22 19:29:22 +0900

    UI

--- src/components/DeepLearning.jsx
+++ src/components/DeepLearning.jsx
@@ -1,2 +1,13 @@
 /* eslint-disable prettier/prettier */
 import React, { useState, useRef } from 'react';
+  return (
+    <div>
+      <h1>画面共有テスト</h1>
+      <video ref={videoRef} style={{ width: '100%' }} autoPlay playsInline></video>
+      {!isSharing ? (
+        <button onClick={startScreenShare}>画面共有開始</button>
+      ) : (
+        <button onClick={stopScreenShare}>画面共有停止</button>
+      )}
+    </div>
+  );
